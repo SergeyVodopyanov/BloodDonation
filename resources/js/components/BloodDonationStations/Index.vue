4fbf95270df588c32d20fb11284ea3312ac384bd
Sergey <vodopyanov03@bk.ru>
2024-10-16 02:47:58 +0300

    Добавил фильтр по городу в StationIndex

--- resources/js/components/BloodDonationStations/Index.vue
+++ resources/js/components/BloodDonationStations/Index.vue
@@ -1,5 +1,23 @@
 <template>
     <div>
+        <div class="mb-3">
+            <label for="cityFilter" class="form-label">Фильтр по городу:</label>
+            <select
+                v-model="selectedCity"
+                class="form-select"
+                id="cityFilter"
+                @change="filterStations"
+            >
+                <option value="">Все города</option>
+                <option
+                    v-for="city in cities"
+                    :key="city.id"
+                    :value="city.cityTitle"
+                >
+                    {{ city.cityTitle }}
+                </option>
+            </select>
+        </div>
         <table class="table">
             <thead>
                 <tr>
@@ -19,7 +37,7 @@
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="station in stations" :key="station.id">
+                <tr v-for="station in filteredStations" :key="station.id">
                     <th scope="row">{{ station.id }}</th>
                     <td>{{ station.bloodDonationStationTitle }}</td>
                     <td>{{ station.city.cityTitle }}</td>
@@ -79,15 +97,18 @@
 
 <script setup>
 import axios from "axios";
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import { useRouter } from "vue-router";
 import api from "../../api";
 
 let router = useRouter();
 
 let stations = ref(null);
+let cities = ref([]);
+let selectedCity = ref("");
 
 getStations(api);
+getCities(api);
 
 function getStations(api) {
     api.get("/api/stations", {
@@ -97,6 +118,17 @@ function getStations(api) {
     }).then((res) => {
         stations.value = res.data.data;
         console.log(stations.value);
+    });
+}
+
+function getCities(api) {
+    api.get("/api/cities", {
+        // headers: {
+        //     Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        // },
+    }).then((res) => {
+        cities.value = res.data.data;
+        console.log(cities.value);
     });
 }
 
@@ -111,5 +143,18 @@ function getBloodGroupStatus(station, bloodGroupTitle) {
             return "Требуется";
         }
     } else return "Неизвестно";
+}
+
+const filteredStations = computed(() => {
+    if (!stations.value) return [];
+    if (!selectedCity.value) return stations.value;
+    return stations.value.filter(
+        (station) => station.city.cityTitle === selectedCity.value
+    );
+});
+
+function filterStations() {
+    // Эта функция вызывается при изменении выбранного города
+    // Вычисление filteredStations будет автоматически обновлено
 }
 </script>
